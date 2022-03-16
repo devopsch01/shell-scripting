542255798d4cfa7bd720fe87fbb61e1074f87b23
chadana <chandana.pdm@gmail.com>
2022-03-16 15:10:03 +0530

    simple script for echo print demo

--- print.sh
+++ print.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 ls
 echo hello world
+
+##color codes
+#Red     31
+#Green   32
+#Yellow  33
+#Blue    34
+#Magenta 35
+#Cyan    36
+
+#syntax : echo -e "\e[COLmHello\e[0m"
+# -e option is to enable esc seq,without -e colors will not work
+# ""Double quotes are mandatory for colors to work,otherwise it will not work,optionally we can use single quotes also,but  way is double quotes
+#\e[COLm -> this to enable the color,COL is one of the color code
+#\e[0m -> this is to disable the color
+
+echo -e "\e[31mtext in Red color"
+echon"one more line"
