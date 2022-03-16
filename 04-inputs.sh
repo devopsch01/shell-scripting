2232b211279d86588a656b69e9ea304999db9c77
chadana <chandana.pdm@gmail.com>
2022-03-16 18:16:59 +0530

    input special variables

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -2,3 +2,11 @@
 
 read -p 'enter your name: ' name
 echo "your name = $name"
+
+
+#special variables
+#$0-$n , $* / $@, $#
+echo script name = $0
+echo first argument = $1
+echo all arguments = $*
+echo number of arguments = $#
