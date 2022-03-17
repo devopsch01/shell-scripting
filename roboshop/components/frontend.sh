277e9ff7779d6ae360fd79cc761ca22e69db3f78
chadana <chandana.pdm@gmail.com>
2022-03-17 16:18:19 +0530

    making frontend components

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+echo -e "\[36m installing Ngnix \e[0m"
 yum install nginx -y
 
-
+echo -e "\[36m downloading Ngnix content\e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
+
+echo -e "\[36m  cleanup old Ngnix content and extract new downloaded archieve \e[0m"
 rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
@@ -13,5 +15,6 @@ mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
+echo -e "\[36m starting Ngnix \e[0m"
 systemctl restart nginx
 systemctl enable nginx
