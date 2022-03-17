05a1694a2a4428227443f0063b7e2161670262fd
chadana <chandana.pdm@gmail.com>
2022-03-17 15:51:32 +0530

    making frontend components

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,6 +6,7 @@ yum install nginx -y
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
 rm -rf /usr/share/nginx/html/*
+cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
 mv frontend-main/* .
 mv static/* .
