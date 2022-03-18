6dc41f6e65c02bd7968e2c25762050f7a85957fe
chadana <chandana.pdm@gmail.com>
2022-03-18 15:40:11 +0530

    condition script

---
+++ 07-condition.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# case and if  are  two conditional commands so if condition is widely use because it has more option than case cpmmand
+# if condition
+# if found in three ways
+# simple if
+# if {expression }
+# then
+# commands
+# f1
+ if [ 1 =eq 1 ]
+then
+    echo hello
+f1
