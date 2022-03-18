5e8d00fd939e76db97b05fc4bfeccf8be2d4a734
chadana <chandana.pdm@gmail.com>
2022-03-18 17:36:05 +0530

    condition script

--- 07-condition.sh
+++ 07-condition.sh
@@ -10,7 +10,21 @@
 # commands
 # fi
 
-if [ 1 -eq 1 ]
-then
-    echo hello
+#expressions are important
+# 1. string tests
+# operators : == , !=,  -z
+# 2. number tests
+# 3. file tests
+
+a="abc"
+if [ "$a" == "abc" ]; then
+  echo  both are equal
+fi
+
+if [ "$a"  != "abc" ]; then
+  echo both are not equal
+fi
+
+if [ -z "$b" ]; then
+  echo b variable is empty
 fi
