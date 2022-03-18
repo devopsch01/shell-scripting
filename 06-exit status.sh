6fdf1b38ae6b7ff045446a04c3d26bcc30bc2b6a
chadana <chandana.pdm@gmail.com>
2022-03-18 16:04:23 +0530

    condition script

--- 06-exit status.sh
+++ 06-exit status.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 
-# exist status is a number.$? is a special variable that holds this value
-# $? is going to have the exit status of last executed command
+# exist status is a number, $? is a special variable that holds this value
+# $? is going to have the exit status of last executed command.
+
 # range of exit status is 0-255
 # 0 -> global success
 # 1-125 -> some failure from the command
