96ee49640fed6754b595f0f91986ea3ac1bee7fd
chadana <chandana.pdm@gmail.com>
2022-03-18 15:52:36 +0530

    condition script

--- 06-exit status.sh
+++ 06-exit status.sh
@@ -4,6 +4,6 @@
 # exist status is a number.$? is a special variable that holds this value
 # $? is going to have the exit status of last executed command
 # range of exit status is 0.255
-# 0-> global success
+# 0 -> global success
 # 1-125 ->some failure from the command
 # 125 -> system failures
