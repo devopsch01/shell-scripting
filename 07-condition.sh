dc83c8ea091a8b6bd8905e7c12fe3ce3c0d4120b
chadana <chandana.pdm@gmail.com>
2022-03-18 18:02:18 +0530

    condition script

--- 07-condition.sh
+++ 07-condition.sh
@@ -5,8 +5,29 @@
 # if found in three forms
 
 # simple if
-# if [expression ]
+
+# if [ expression ]
 # then
+# commands
+# fi
+
+# if else
+
+#if [ expression ]; then
+# commands
+# else
+# commands
+# fi
+
+# else if
+
+# if [ expression ]; then
+# commands
+# elsif [ expressions2 ]; then
+# commands
+# elsif [ expression3 ]; then
+# commands
+# else
 # commands
 # fi
 
@@ -28,3 +49,13 @@ fi
 if [ -z "$b" ]; then
   echo b variable is empty
 fi
+
+## one observation,i am using quotes for variables and it is a best practice
+
+if [ "$a" == "abc" ]; then
+  echo  both are equal
+else
+  echo both are not equal
+fi
+
+
